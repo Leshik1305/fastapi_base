0dff0f25c3735e45d8c363ce5764290ecdeb5949
Leshik1305 <realleshik1305@gmail.com>
2025-10-16 23:32:34 +0300

    add get and post users

---
+++ fastapi-aplication/api/api_v1/users.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from core.schemas.user import UserRead, UserCreate
+from crud import users as users_crud
+
+router = APIRouter(tags=["Users"])
+
+
+@router.get("/", response_model=list[UserRead])
+async def get_users(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    users = await users_crud.get_all_users(session=session)
+    return users
+
+
+@router.post("/", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+async def create_user(
+    user_create: UserCreate,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    user = await users_crud.create_user(
+        session=session,
+        user_create=user_create,
+    )
+    return user
